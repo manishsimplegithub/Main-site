ddd5f8a45cc892d4a6b7f0de7ea07bd509a45cd5
manishsimplegithub <manishsrivastav@bbdu.ac.in>
2022-12-13 00:12:43 +0530

    changes

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -20,7 +20,7 @@ const Contact = () => {
   const onSubmits = (e) => {
     e.preventDefault();
     alert(
-      `My name is ${data.fullname}<br>my mobile number is ${data.number}<br>my email is ${data.Email} <br>write any message ${data.msg}`
+      ` My name is ${data.fullname}\n my mobile number is ${data.number}\n my email is ${data.Email}\n write any message ${data.msg}`
     );
   };
 
